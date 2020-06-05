aa88d5920957846ef4f73bc5f9fdd75d2124d13e
victormdev <victoralexandrino99@gmail.com>
2020-06-05 13:39:03 -0300

    criando arquivo de template externo

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,20 +1,10 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
-import 'time.dart';
+import 'src/time.dart';
 
 @Component(
   selector: 'my-app',
-  template: '''
-  <h1>{{title}}</h1>
-  <h2>{{time.name}}</h2>
-  <div><label>id: </label>{{time.id}}</div>
-  <div><label>name: </label>{{time.name}}</div>
-  <div><label>ano: </label>{{time.ano}}</div>
-  <div>
-    <label>name: </label>
-    <input [(ngModel)]="time.name" placeholder="Digite o nome do time aqui">
-  </div>
-  ''',
+  templateUrl: 'app_component.html',
   directives: [formDirectives],
 )
 class AppComponent {
--- lib/time.dart
+++
@@ -1,6 +0,0 @@
-class Time {
-  final int id, ano;
-  String name;
-
-  Time(this.id, this.name, this.ano);
-}
