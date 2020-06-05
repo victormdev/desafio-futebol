febfc6ff7823601052c674ef9c8a9f3568f1ef92
victormdev <victoralexandrino99@gmail.com>
2020-06-05 13:42:24 -0300

    criando mock de times

---
+++ lib/src/mock_times.dart
@@ -0,0 +1,9 @@
+import 'time.dart';
+
+final mockTimes = <Time>[
+  Time(11, 'Bahia', 1888),
+  Time(12, 'São Paulo', 2000),
+  Time(13, 'Palmeiras', 2000),
+  Time(14, 'Vitória', 2000),
+  Time(15, 'Corinthins', 1910)
+];
