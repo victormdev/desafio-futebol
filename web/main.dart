25e78777e39596b28483174f11d0d9f2ca2d35d8
victormdev <victoralexandrino99@gmail.com>
2020-06-05 22:03:13 -0300

    adicionando angular_router e criando o service "falso"

--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,18 @@
 import 'package:angular/angular.dart';
 import 'package:angular_app/app_component.template.dart' as ng;
+import 'package:angular_app/in_memory_data_service.dart';
+import 'package:angular_router/angular_router.dart';
+import 'package:http/browser_client.dart';
+import 'package:http/http.dart';
+
+@GenerateInjector([
+  routerProvidersHash, // You can use routerProviders in production
+  ClassProvider(Client, useClass: InMemoryDataService),
+  // Using a real back end?
+  // Import 'package:http/browser_client.dart' and change the above to:
+  //   ClassProvider(Client, useClass: BrowserClient),
+])
+final InjectorFactory injector = self.injector$Injector;
 
 void main() {
-  runApp(ng.AppComponentNgFactory);
-}
+  runApp(ng.AppComponentNgFactory, createInjector: injector);
