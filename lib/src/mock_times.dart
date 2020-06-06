9508a2eb2bab67260d4ca2025ba1f6bc531da93b
victormdev <victoralexandrino99@gmail.com>
2020-06-05 23:08:42 -0300

    deletando mock_times

--- lib/src/mock_times.dart
+++
@@ -1,9 +0,0 @@
-import 'time.dart';
-
-final mockTimes = <Time>[
-  Time(1, 'Bahia', 'Bahia', 1888),
-  Time(2, 'São Paulo', 'São Paulo', 2000),
-  Time(3, 'São Paulo', 'Palmeiras', 2000),
-  Time(4, 'Bahia', 'Vitória', 2000),
-  Time(5, 'São Paulo', 'Corinthins', 1910)
-];
